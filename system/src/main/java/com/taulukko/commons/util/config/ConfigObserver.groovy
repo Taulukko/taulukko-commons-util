783ec5853c73ff701c6b02b2167942890c54866a
gandb <gandbranco@gmail.com>
2018-07-09 22:34:21 -0300

    Remoçao parcial do groovy

--- system/src/main/java/com/taulukko/commons/util/config/ConfigObserver.groovy
+++
@@ -1,7 +0,0 @@
-package com.taulukko.commons.util.config;
-
-interface ConfigObserver {
-	void before();
-
-	void after();
-}
---
+++ system/src/main/java/com/taulukko/commons/util/config/ConfigObserver.java
@@ -0,0 +1,7 @@
+package com.taulukko.commons.util.config;
+
+public interface ConfigObserver {
+	public void before();
+
+	public void after();
+}
